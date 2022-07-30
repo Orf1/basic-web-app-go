1ee672b9afd608833f70ed06c448b3521dea6dbf
Orf1 <orfeas126@gmail.com>
2022-07-29 17:31:55 -0700

    Added template caching

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"fmt"
+	"github.com/orf1/basic-web-app-go/pkg/config"
 	"github.com/orf1/basic-web-app-go/pkg/handlers"
+	"github.com/orf1/basic-web-app-go/pkg/render"
 	"log"
 	"net/http"
 )
@@ -10,9 +11,18 @@ import (
 const port = ":8080"
 
 func main() {
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template ceche")
+	}
+
+	app.TemplateCache = tc
+
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 
-	log.Println(fmt.Sprintf("Listening on port %s", port))
+	log.Printf("Listening on port %s", port)
 	_ = http.ListenAndServe(port, nil)
 }
